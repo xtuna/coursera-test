da192834a4187e60c18b2c2b3a742711f638c8c2
xtuna <92073982+xtuna@users.noreply.github.com>
2023-11-16 11:00:21 +0800

    Add files via upload

--- module4-solution/js/script.js
+++ module4-solution/js/script.js
@@ -1,1 +1,14 @@
+(function () {
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
+    for (var i = 0; i < names.length; i++) {
+        var currentName = names[i];
+        var firstLetter = currentName.charAt(0).toLowerCase();
+
+        if (firstLetter === 'j') {
+            byeSpeaker.speak(currentName);
+        } else {
+            helloSpeaker.speak(currentName);
+        }
+    }
+})();
